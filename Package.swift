d8c3fb32ce2ba239cfcdc627d68430f804761685
Pedro Sakey <pedrosakey@gmail.com>
2022-06-21 12:05:40 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,17 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "MailCore2",
+    name: "MailCore",
     platforms: [
         .iOS(.v9), .macOS(.v10_10)
     ],
     products: [
         .library(
-            name: "MailCore2",
-            targets: ["MailCore2"]),
+            name: "MailCore",
+            targets: ["MailCore"]),
     ],
     targets: [
-        .binaryTarget(name: "MailCore2",
+        .binaryTarget(name: "MailCore",
                       url: "https://github.com/pedrosakey/files/raw/main/mailCore.xcframework.zip",
                       checksum: "1ca4bc9755fdaf8b66ec912a6afe0ba2b3d5830d0912567a3e7686bdeb7a9329")
     ]
