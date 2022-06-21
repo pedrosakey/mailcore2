547793c336ac7a69e742fa441b89b36a83301bda
Pedro Sakey <pedrosakey@gmail.com>
2022-06-21 12:00:13 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "MailCore2",
-                      url: "https://github.com/pedrosakey/files/raw/main/MailCore2.framework.zip",
+                      url: "https://github.com/pedrosakey/files/raw/main/mailCore.xcframework.zip",
                       checksum: "0aae9189868edadde34ae9df46a913a1c55ed1729cd080b2c6009960489c4f40")
     ]
 )
